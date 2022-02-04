c671277e2c34c5d85507ad1b22bdc0f25053b83a
mendes114 <mauriciomendees@gmail.com>
2022-02-04 03:49:27 -0300

    80% conclused 220204 03:49am

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,31 @@ tns({
 				items: 3,
 			},
 			992: {
-				items: 4,
+				items: 6,
+			}
+		}
+	});
+	//======== tiny slider
+tns({
+		container: '.client-logo-carousel2',
+		autoplay: true,
+		autoplayButtonOutput: false,
+		mouseDrag: true,
+		gutter: 15,
+		nav: false,
+		controls: false,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			540: {
+				items: 2,
+			},
+			768: {
+				items: 3,
+			},
+			992: {
+				items: 6,
 			}
 		}
 	});
